95c858cc35fd21827dcc62a5db396df57a6a6d2b
Nam Pham <phamnam3@hotmail.com>
2021-09-29 13:30:52 -0400

    Created Restaurants page

--- ClientApp/src/App.tsx
+++ ClientApp/src/App.tsx
@@ -1,5 +1,22 @@
 import React from 'react'
+import { Restaurants } from './pages/Restaurants'
 
 export function App() {
-  return <div>Hello, World </div>
+  return (
+    <div>
+      <header>
+        <h1>Re-entrée</h1>
+        <div>
+          <button>Sign In</button>
+          <button>Sign Up</button>
+        </div>
+      </header>
+
+      <Restaurants />
+
+      <footer>
+        <p>Re-entrée by Nam Pham</p>
+      </footer>
+    </div>
+  )
 }
---
+++ ClientApp/src/pages/Restaurants.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export function Restaurants() {
+  return (
+    <>
+      <section>MAP GOES HERE</section>
+      <form>
+        <input type="text" placeholder="" />
+      </form>
+      <ul>
+        <li>
+          <h2>Casita</h2>
+          <h3>Antos, tacos, another favorite</h3>
+        </li>
+        <li>
+          <h2>The Burg</h2>
+          <h3>Single grilled, favorite 2, favorite 3</h3>
+        </li>
+        <li>
+          <h2>Bodega</h2>
+          <h3>Lechon sandwich, favorite 2, favorite 3</h3>
+        </li>
+      </ul>
+    </>
+  )
+}
