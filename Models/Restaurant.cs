1725dc366063b156bf90a6d24ab4838ed3ed749f
Nam Pham <phamnam3@hotmail.com>
2021-10-04 16:31:35 -0400

    Created Restaurant model and database

---
+++ Models/Restaurant.cs
@@ -0,0 +1,12 @@
+namespace Capstone.Models
+{
+    public class Restaurant
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Website { get; set; }
+        public string Comments { get; set; }
+    }
+}
