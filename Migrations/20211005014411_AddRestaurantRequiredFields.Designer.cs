831f4a25bcbf9fed7816c864c8a8a50c6a0cffe9
Nam Pham <phamnam3@hotmail.com>
2021-10-04 21:45:44 -0400

    Added requirements for Name and Address in Restaurant model Updated database

--- Migrations/20211005014411_AddRestaurantRequiredFields.Designer.cs
+++ Migrations/20211005014411_AddRestaurantRequiredFields.Designer.cs
@@ -2,15 +2,17 @@
 using Capstone.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace Capstone.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20211005014411_AddRestaurantRequiredFields")]
+    partial class AddRestaurantRequiredFields
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Models/Restaurant.cs
+++ Models/Restaurant.cs
@@ -1,9 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Capstone.Models
 {
     public class Restaurant
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage="Name required.")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage="Address required.")]
+
         public string Address { get; set; }
         public string Phone { get; set; }
         public string Website { get; set; }
