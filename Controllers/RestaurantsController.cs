4b62bad20d7526ceef114ea276a15da98f67149f
Nam Pham <phamnam3@hotmail.com>
2021-10-04 23:48:29 -0400

    Changed RestaurantsControllers Get (id) to include Favorites

--- Controllers/RestaurantsController.cs
+++ Controllers/RestaurantsController.cs
@@ -48,7 +48,7 @@ namespace Capstone.Controllers
         public async Task<ActionResult<Restaurant>> GetRestaurant(int id)
         {
             // Find the restaurant in the database using `FindAsync` to look it up by id
-            var restaurant = await _context.Restaurants.FindAsync(id);
+            var restaurant = await _context.Restaurants.Include(restaurant => restaurant.Reviews).Where(restaurant => restaurant.Id == id).FirstOrDefaultAsync();
 
             // If we didn't find anything, we receive a `null` in return
             if (restaurant == null)
