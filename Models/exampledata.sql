da1c057e2c4facc52d77b7848e2501b6cc31436a
Nam Pham <phamnam3@hotmail.com>
2021-10-04 23:28:09 -0400

    Added data for Favorites table

--- Models/exampledata.sql
+++ Models/exampledata.sql
@@ -3,3 +3,6 @@ TRUNCATE TABLE "Restaurants" RESTART IDENTITY;
 INSERT INTO "Restaurants" ("Name", "Address", "Phone", "Website", "Comments") VALUES ('Casita Taqueria', '2663 Central Ave, St. Petersburg, FL 33713', '727-498-8749', 'casitatacos.com', 'lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum');
 INSERT INTO "Restaurants" ("Name", "Address", "Phone", "Website", "Comments") VALUES ('The Burg Bar & Grill', '1752 Central Ave, St. Petersburg, FL 33712', '727-894-2874', 'theburgbar.com', 'lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum');
 INSERT INTO "Restaurants" ("Name", "Address", "Phone", "Website", "Comments") VALUES ('Bodega on Central', '1180 Central Ave, St. Petersburg, FL 33705', '727-623-0942', 'eatatbodega.com', 'lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum');
+
+INSERT INTO "Favorites" ("RestaurantId", "Name", "Price") VALUES (1, "Barbacoa Tortas", 8.50);
+INSERT INTO "Favorites" ("RestaurantId", "Name", "Price") VALUES (1, "Carne Asada Casita Bowl", 8.99);
